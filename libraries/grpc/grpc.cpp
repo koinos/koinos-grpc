62d9297b8cd87fe55f5cfb2df8e9101d0d4f81ed
Steve Gerbino <steve@gerbino.co>
2023-01-19 16:40:23 -0500

    #4: Initial implementation of whitelist and blacklist

--- libraries/grpc/grpc.cpp
+++ libraries/grpc/grpc.cpp
@@ -20,12 +20,18 @@
 const ::koinos::rpc::svc::BOOST_PP_CAT(method, _request*) request,                      \
 ::koinos::rpc::svc::BOOST_PP_CAT(method, _response*) response )                         \
 {                                                                                       \
+   const auto& [ permitted, msg ] = call_permitted( #svc, #method );                    \
+   if ( !permitted )                                                                    \
+      return ::grpc::Status( ::grpc::StatusCode::PERMISSION_DENIED, msg );              \
+                                                                                        \
    try                                                                                  \
    {                                                                                    \
       rpc::svc::BOOST_PP_CAT( svc, _request ) req;                                      \
       BOOST_PP_CAT(*req.mutable_, method)() = *request;                                 \
-      auto future = _client.rpc( util::service::svc,                                    \
-         util::converter::as< std::string >( req ), _timeout, mq::retry_policy::none ); \
+      auto future = _config.client.rpc( util::service::svc,                             \
+         util::converter::as< std::string >( req ),                                     \
+         _config.timeout,                                                               \
+         mq::retry_policy::none );                                                      \
       rpc::svc::BOOST_PP_CAT( svc, _response ) resp;                                    \
       resp.ParseFromString( future.get() );                                             \
       KOINOS_ASSERT( BOOST_PP_CAT( resp.has_, method )(),                               \
@@ -57,6 +63,18 @@ using namespace std::chrono_literals;
 
 namespace koinos::services {
 
+std::pair< std::string, std::string > split_list_entry( const std::string& entry )
+{
+   static const std::string delim{ "." };
+
+   auto delim_pos = entry.find( delim );
+
+   if ( delim_pos == std::string::npos )
+      return std::make_pair( entry, std::string{} );
+
+   return std::make_pair( entry.substr( 0, delim_pos ), entry.substr( delim_pos + 1, entry.size() ) );
+}
+
 // Global callback implementation
 
 callbacks::callbacks( std::atomic< uint64_t >& req_count ) : _request_count( req_count ) {}
@@ -71,7 +89,60 @@ void callbacks::PostSynchronousRequest( grpc_impl::ServerContext* context ) {}
 
 // Mempool service implementation
 
-mempool_service::mempool_service( mq::client& c, std::chrono::seconds timeout ) : _client( c ), _timeout( timeout ) {}
+mempool_service::mempool_service( configuration& cfg ) : _config( cfg ) {}
+
+std::pair< bool, std::string > mempool_service::call_permitted( const std::string& service, const std::string& method )
+{
+   if ( _config.whitelist.size() )
+   {
+      bool in_whitelist = false;
+
+      for ( const auto& entry : _config.whitelist )
+      {
+         const auto& [ svc, fn ] = split_list_entry( entry );
+
+         // Entire service was whitelist
+         if ( service == svc )
+         {
+            if ( fn.empty() )
+            {
+               in_whitelist = true;
+               break;
+            }
+         }
+         // Check if this particular method is whitelisted
+         else if ( method == fn )
+         {
+            in_whitelist = true;
+            break;
+         }
+      }
+
+      if ( !in_whitelist )
+         return std::make_pair( false, "method not whitelisted" );
+   }
+
+   for ( const auto& entry : _config.blacklist )
+   {
+      const auto& [ svc, fn ] = split_list_entry( entry );
+
+      // Entire service was whitelist
+      if ( service == svc )
+      {
+         if ( fn.empty() )
+         {
+            return std::make_pair( false, "method is blacklisted" );
+         }
+      }
+      // Check if this particular method is whitelisted
+      else if ( method == fn )
+      {
+         return std::make_pair( false, "method is blacklisted" );
+      }
+   }
+
+   return std::make_pair( true, std::string{} );
+}
 
 GRPC_SYNC_METHOD_DEFINITIONS( mempool,
    (get_pending_transactions)
@@ -80,7 +151,60 @@ GRPC_SYNC_METHOD_DEFINITIONS( mempool,
 
 // Account history implementation
 
-account_history_service::account_history_service( mq::client& c, std::chrono::seconds timeout ) : _client( c ), _timeout( timeout ) {}
+account_history_service::account_history_service( configuration& cfg ) : _config( cfg ) {}
+
+std::pair< bool, std::string > account_history_service::call_permitted( const std::string& service, const std::string& method )
+{
+   if ( _config.whitelist.size() )
+   {
+      bool in_whitelist = false;
+
+      for ( const auto& entry : _config.whitelist )
+      {
+         const auto& [ svc, fn ] = split_list_entry( entry );
+
+         // Entire service was whitelist
+         if ( service == svc )
+         {
+            if ( fn.empty() )
+            {
+               in_whitelist = true;
+               break;
+            }
+         }
+         // Check if this particular method is whitelisted
+         else if ( method == fn )
+         {
+            in_whitelist = true;
+            break;
+         }
+      }
+
+      if ( !in_whitelist )
+         return std::make_pair( false, "method not whitelisted" );
+   }
+
+   for ( const auto& entry : _config.blacklist )
+   {
+      const auto& [ svc, fn ] = split_list_entry( entry );
+
+      // Entire service was whitelist
+      if ( service == svc )
+      {
+         if ( fn.empty() )
+         {
+            return std::make_pair( false, "method is blacklisted" );
+         }
+      }
+      // Check if this particular method is whitelisted
+      else if ( method == fn )
+      {
+         return std::make_pair( false, "method is blacklisted" );
+      }
+   }
+
+   return std::make_pair( true, std::string{} );
+}
 
 GRPC_SYNC_METHOD_DEFINITIONS( account_history,
    (get_account_history)
--- tests/tests/services_test.cpp
+++ tests/tests/services_test.cpp
@@ -40,6 +40,31 @@ struct grpc_fixture
 
 BOOST_FIXTURE_TEST_SUITE( grpc_tests, grpc_fixture )
 
+BOOST_AUTO_TEST_CASE( split_entry_test )
+{
+   std::vector< std::string > list{ "service.method1", "service.method2", "service", "service.", ".method" };
+
+   auto [ svc1, method1 ] = koinos::services::split_list_entry( list[0] );
+   BOOST_REQUIRE( svc1 == "service" );
+   BOOST_REQUIRE( method1 == "method1" );
+
+   auto [ svc2, method2 ] = koinos::services::split_list_entry( list[1] );
+   BOOST_REQUIRE( svc2 == "service" );
+   BOOST_REQUIRE( method2 == "method2" );
+
+   auto [ svc3, method3 ] = koinos::services::split_list_entry( list[2] );
+   BOOST_REQUIRE( svc3 == "service" );
+   BOOST_REQUIRE( method3.empty() );
+
+   auto [ svc4, method4 ] = koinos::services::split_list_entry( list[3] );
+   BOOST_REQUIRE( svc4 == "service" );
+   BOOST_REQUIRE( method4.empty() );
+
+   auto [ svc5, method5 ] = koinos::services::split_list_entry( list[4] );
+   BOOST_REQUIRE( svc5.empty() );
+   BOOST_REQUIRE( method5 == "method" );
+}
+
 BOOST_AUTO_TEST_CASE( grpc_basic_test )
 {
 }
