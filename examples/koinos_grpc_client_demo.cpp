74199e1d0b234e68c08be27f33b9ddeaaf680cc5
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2024-03-13 11:42:33 -0600

    Format client_demo

--- examples/koinos_grpc_client_demo.cpp
+++ examples/koinos_grpc_client_demo.cpp
@@ -1,5 +1,5 @@
-#include <grpcpp/security/server_credentials.h>
 #include <grpcpp/grpcpp.h>
+#include <grpcpp/security/server_credentials.h>
 
 #include <koinos/account_history/account_history.grpc.pb.h>
 #include <koinos/services/grpc.hpp>
@@ -9,33 +9,34 @@
 
 int main( int argc, char** argv )
 {
-   std::string target_address("0.0.0.0:50051");
-   auto channel = ::grpc::CreateChannel( target_address, grpc::InsecureChannelCredentials() );
+  std::string target_address( "0.0.0.0:50051" );
+  auto channel = ::grpc::CreateChannel( target_address, grpc::InsecureChannelCredentials() );
 
-   std::unique_ptr< koinos::services::koinos::Stub > stub( koinos::services::koinos::NewStub( channel ) );
+  std::unique_ptr< koinos::services::koinos::Stub > stub( koinos::services::koinos::NewStub( channel ) );
 
-   koinos::rpc::account_history::get_account_history_request request;
+  koinos::rpc::account_history::get_account_history_request request;
 
-   request.set_address( koinos::util::from_base58< std::string >( std::string{ "1NsQbH5AhQXgtSNg1ejpFqTi2hmCWz1eQS" } ) );
-   request.set_limit( 10 );
+  request.set_address(
+    koinos::util::from_base58< std::string >( std::string{ "1NsQbH5AhQXgtSNg1ejpFqTi2hmCWz1eQS" } ) );
+  request.set_limit( 10 );
 
-   // Container for server response
-   koinos::rpc::account_history::get_account_history_response response;
-   // Context can be used to send meta data to server or modify RPC behaviour
-   grpc::ClientContext context;
+  // Container for server response
+  koinos::rpc::account_history::get_account_history_response response;
+  // Context can be used to send meta data to server or modify RPC behaviour
+  grpc::ClientContext context;
 
-   // Actual Remote Procedure Call
-   grpc::Status status = stub->get_account_history( &context, request, &response );
+  // Actual Remote Procedure Call
+  grpc::Status status = stub->get_account_history( &context, request, &response );
 
-   // Returns results based on RPC status
-   if ( status.ok() )
-   {
-      std::cout << response;
-   }
-   else
-   {
-      std::cout << status.error_code() << ": " << status.error_message() << std::endl;
-   }
+  // Returns results based on RPC status
+  if( status.ok() )
+  {
+    std::cout << response;
+  }
+  else
+  {
+    std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+  }
 
-   return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
